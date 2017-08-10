a1d0d314299e1aa7b003123be6c825aec44578c8
Jonathan Peres <jonathan.peres@protonmail.com>
2017-08-10 12:22:08 -0300

    Update yaml script

--- yaml.sh
+++ yaml.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
 function parse_yaml() {
+    local yaml_file=$1
     local prefix=$2
     local s
     local w
     local fs
 
     s='[[:space:]]*'
-    w='[a-zA-Z0-9_\-]*'
+    w='[a-zA-Z0-9_]*'
     fs="$(echo @|tr @ '\034')"
 
-    cat "$1" |
-    sed 's/\"/\\\"/g' |
-    sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
-        -e "s|^\($s\)\($w\)$s[:-]$s\(.*\)$s\$|\1$fs\2$fs\3|p" |
-    awk -F"$fs" '{
-        indent = length($1)/2;
-        vname[indent] = $2;
-        for (i in vname) {if (i > indent) {delete vname[i]}}
-            if (length($3) > 0) {
-                vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
-                printf("%s%s%s=(\"%s\")\n", "'"$prefix"'",vn, $2, $3);
-            }
-        }' |
-    sed 's/_=/+=/g'
+    (
+        sed 's/\"/\\\"/g' |
+        sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
+            -e "s|^\($s\)\($w\)$s[:-]$s\(.*\)$s\$|\1$fs\2$fs\3|p" |
+        awk -F"$fs" '{
+            indent = length($1)/2;
+            if (length($2) == 0) { conj[indent]="+";} else {conj[indent]="";}
+            vname[indent] = $2;
+            for (i in vname) {if (i > indent) {delete vname[i]}}
+                if (length($3) > 0) {
+                    vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+                    printf("%s%s%s%s=(\"%s\")\n", "'"$prefix"'",vn, $2, conj[indent-1],$3);
+                }
+            }' |
+        sed 's/_=/+=/g'
+    ) < "$yaml_file"
 }
 
 function create_variables() {
